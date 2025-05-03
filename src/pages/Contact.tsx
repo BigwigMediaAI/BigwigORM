b9b835621cc7e71f810ab92fbc0ed31b314ba81c
unknown <chandanpms@gmail.com>
2025-05-03 11:35:18 +0530

    updated contact and ceo image

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -133,10 +133,18 @@ export default function ContactPage() {
           </p>
 
           <div className="flex flex-col gap-4">
+            <div className="bg-white rounded-md p-4 flex items-center gap-4 shadow-sm hover:bg-gray-200 transition font-sans">
+              <Phone className="w-6 h-6 text-gray-700" />
+              <span className="text-sm text-gray-700">
+                +971 502615979 (Dubai)
+              </span>
+            </div>
             {/* Phone Button */}
             <div className="bg-white rounded-md p-4 flex items-center gap-4 shadow-sm hover:bg-gray-200 transition font-sans">
               <Phone className="w-6 h-6 text-gray-700" />
-              <span className="text-sm text-gray-700">+91 83685 73451</span>
+              <span className="text-sm text-gray-700">
+                +91 83685 73451 (India)
+              </span>
             </div>
 
             {/* WhatsApp Button */}
